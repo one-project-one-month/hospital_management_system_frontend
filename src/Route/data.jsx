b682d5450e7864f042928cc8526cd9b58d6965e5
IvanStark <104026643+AzrealStillCute@users.noreply.github.com>
2024-03-16 19:32:14 +0630

    configure route and layout

--- src/Route/data.jsx
+++ src/Route/data.jsx
@@ -1,8 +1,45 @@
+import NotFound from '@/components/NotFound/NotFound.jsx'
+import Main from '../Layout/Main/Main.jsx'
 import { Home } from '../Pages/Home/Home.jsx'
 
 export const routes = [
     {
         path: "/",
-        element: <Home />
-    }
+        Component: Main,
+        errorElement: <NotFound/>,
+        children: [
+            {
+                index: true,
+                Component: Home,
+            },
+            {
+                path: "doctor",
+                children: [
+                    {
+                        // index: true,
+                        path: "doctorList",
+                        element: <h1>Doctor List</h1>,
+                    },
+                    {
+                        path: "doctorSpecialist",
+                        element: <h1>Doctor Specialist</h1>,
+                    }
+                ]
+            },
+            {
+                path: "patient",
+                children: [
+                    {
+                        // index: true,
+                        path: "patientList",
+                        element: <h1>Patient List</h1>
+                    },
+                    {
+                        path: "medicalRecords",
+                        element: <h1>Medical Records</h1>,
+                    }
+                ]
+            }
+        ]
+    },
 ]
