b90706f9fdb794fdfb691e7fed947b5515f58ead
IvanStark <104026643+AzrealStillCute@users.noreply.github.com>
2024-03-18 09:01:45 +0630

    remvoe tooltip

--- src/components/ThemeToggle/ThemeToggle.jsx
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -1,11 +1,5 @@
 import { Sun, MoonStar } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import { useState } from "react";
 import { useEffect } from "react";
 
@@ -17,7 +11,7 @@ function ThemeToggle() {
   useEffect(() => {
     const root = window.document.documentElement;
 
-    root.classList.remove("light", "dark")
+    root.classList.remove("light", "dark");
 
     if (!localStorage.getItem("ui-theme")) {
       const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
@@ -25,43 +19,34 @@ function ThemeToggle() {
         ? "dark"
         : "light";
 
-      root.classList.add(systemTheme)
+      root.classList.add(systemTheme);
     }
 
-    root.classList.add(theme)
+    root.classList.add(theme);
   }, [theme]);
 
   function setUiTheme(theme) {
-    localStorage.setItem("ui-theme", theme)
+    localStorage.setItem("ui-theme", theme);
     setTheme(theme);
   }
 
   function themeToggle() {
     if (theme === "light") {
-      setUiTheme("dark")
+      setUiTheme("dark");
     } else if (theme === "dark") {
-      setUiTheme("light")
+      setUiTheme("light");
     }
   }
 
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger>
-          <Button
-            onClick={themeToggle}
-            variant="ghost"
-            size="icon"
-            className="w-8 h-8"
-          >
-            {theme === "light" ? <Sun size={16} /> : <MoonStar size={16} />}
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>{theme[0].toUpperCase() + theme.slice(1)} Mode</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <Button
+      onClick={themeToggle}
+      variant="ghost"
+      size="icon"
+      className="w-8 h-8"
+    >
+      {theme === "light" ? <Sun size={16} /> : <MoonStar size={16} />}
+    </Button>
   );
 }
 
