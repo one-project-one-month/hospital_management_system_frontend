4c10ad9f3ecf8cc2e92adab819c643bfa107eed0
htooa2014 <htooak2014@gmail.com>
2024-03-25 21:10:26 +0630

    general setup ui finish

--- src/Route/data.jsx
+++ src/Route/data.jsx
@@ -1,8 +1,18 @@
 import { Home } from '../Pages/Home/Home.jsx'
+import { Room } from '../Pages/Room/Room.jsx'
+import { Disease } from '../Pages/Disease/Disease.jsx'
 
 export const routes = [
     {
         path: "/",
         element: <Home />
+    },
+    {
+        path: "/Room",
+        element: <Room />
+    },
+    {
+        path: "/Disease",
+        element: <Disease />
     }
 ]
